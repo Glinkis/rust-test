8f180607367c5bdfaa980f2202f4931d79556cfd
Victor Glindås <glinkis@gmail.com>
2019-12-12 22:04:43 +0100

    Build a simple program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
+
+fn create_vector2() -> Vector2 {
+    Vector2 { x: 0.0, y: 0.0 }
+}
+
+fn print_vector2(vector: &Vector2) {
+    println!("The vector is: {} {}", vector.x, vector.y);
+}
+
+fn increment_vector2(vector: &mut Vector2) {
+    vector.x += 1.0;
+    vector.y += 1.0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut vector = create_vector2();
+
+    print_vector2(&vector);
+    increment_vector2(&mut vector);
+    print_vector2(&vector);
 }
