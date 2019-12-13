3e2b52bf05e74f36ff7109e73ff8d9980d8836cd
Victor Glindås <glinkis@gmail.com>
2019-12-13 11:59:57 +0100

    Add editorconfig

--- src/vector.rs
+++ src/vector.rs
@@ -1,23 +1,23 @@
 pub struct Vector2 {
-	pub x: f32,
-	pub y: f32,
+    pub x: f32,
+    pub y: f32,
 }
 
 impl Vector2 {
-	pub fn add(&mut self, vector: &Vector2) {
-		self.x += vector.x;
-		self.y += vector.y;
-	}
-	pub fn sub(&mut self, vector: &Vector2) {
-		self.x -= vector.x;
-		self.y -= vector.y;
-	}
-	pub fn mul(&mut self, vector: &Vector2) {
-		self.x *= vector.x;
-		self.y *= vector.y;
-	}
-	pub fn div(&mut self, vector: &Vector2) {
-		self.x /= vector.x;
-		self.y /= vector.y;
-	}
+    pub fn add(&mut self, vector: &Vector2) {
+        self.x += vector.x;
+        self.y += vector.y;
+    }
+    pub fn sub(&mut self, vector: &Vector2) {
+        self.x -= vector.x;
+        self.y -= vector.y;
+    }
+    pub fn mul(&mut self, vector: &Vector2) {
+        self.x *= vector.x;
+        self.y *= vector.y;
+    }
+    pub fn div(&mut self, vector: &Vector2) {
+        self.x /= vector.x;
+        self.y /= vector.y;
+    }
 }
