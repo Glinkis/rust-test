0189733636ad60b17dbb15574fd4848a08294bac
Victor Glindås <glinkis@gmail.com>
2019-12-13 13:10:49 +0100

    Fix broken build

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ mod point;
 use point::Point;
 
 fn create_point() -> Point {
-    Point { x: 0.0, y: 0.0 }
+    Point {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    }
 }
 
 fn print_point(vector: &Point) {
-    println!("The vector is: {} {}", vector.x, vector.y);
+    println!("The vector is: {} {} {}", vector.x, vector.y, vector.z);
 }
 
 fn increment_point(vector: &mut Point) {
     vector.x += 1.0;
     vector.y += 1.0;
+    vector.z += 1.0;
 }
 
 fn main() {
@@ -20,5 +25,6 @@ fn main() {
     print_point(&vector);
     increment_point(&mut vector);
     print_point(&vector);
+    increment_point(&mut vector);
     print_point(&vector);
 }
