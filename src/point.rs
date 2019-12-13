cd913c3340e475cf76162fa48c9d70c836f4fbea
Victor Glindås <glinkis@gmail.com>
2019-12-13 13:07:12 +0100

    Add z dimension

--- src/point.rs
+++ src/point.rs
@@ -1,23 +1,28 @@
 pub struct Point {
     pub x: f32,
     pub y: f32,
+    pub z: f32,
 }
 
 impl Point {
     pub fn add(&mut self, point: &Point) {
         self.x += point.x;
         self.y += point.y;
+        self.z += point.z;
     }
     pub fn sub(&mut self, point: &Point) {
         self.x -= point.x;
         self.y -= point.y;
+        self.z -= point.z;
     }
     pub fn mul(&mut self, point: &Point) {
         self.x *= point.x;
         self.y *= point.y;
+        self.z *= point.z;
     }
     pub fn div(&mut self, point: &Point) {
         self.x /= point.x;
         self.y /= point.y;
+        self.z /= point.z;
     }
 }
