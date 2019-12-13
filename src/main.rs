19bf373d0186bb3dcf93c6aca22dbdaeb792019a
Victor Glindås <glinkis@gmail.com>
2019-12-13 11:56:15 +0100

    Create vector module

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-struct Vector2 {
-    x: f32,
-    y: f32,
-}
+mod vector;
+use vector::Vector2;
 
 fn create_vector2() -> Vector2 {
-    Vector2 { x: 0.0, y: 0.0 }
+	Vector2 { x: 0.0, y: 0.0 }
 }
 
 fn print_vector2(vector: &Vector2) {
-    println!("The vector is: {} {}", vector.x, vector.y);
+	println!("The vector is: {} {}", vector.x, vector.y);
 }
 
 fn increment_vector2(vector: &mut Vector2) {
-    vector.x += 1.0;
-    vector.y += 1.0;
+	vector.x += 1.0;
+	vector.y += 1.0;
 }
 
 fn main() {
-    let mut vector = create_vector2();
+	let mut vector = create_vector2();
 
-    print_vector2(&vector);
-    increment_vector2(&mut vector);
-    print_vector2(&vector);
+	print_vector2(&vector);
+	increment_vector2(&mut vector);
+	print_vector2(&vector);
+	print_vector2(&vector);
 }
